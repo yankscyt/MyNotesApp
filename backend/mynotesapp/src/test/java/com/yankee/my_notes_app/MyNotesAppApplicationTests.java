ce4f2f393bac46009a59cca36ef7a1fd02384c83
yankscyt <yankscaburnay@gmail.com>
2025-12-06 15:32:23 +0800

    fix: Final correction of Java package folder and added Spring Boot plugin

---
+++ backend/mynotesapp/src/test/java/com/yankee/my_notes_app/MyNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yankee.my_notes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
