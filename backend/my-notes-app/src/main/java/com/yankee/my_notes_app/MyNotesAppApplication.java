248c5c0dbd590bc5bc8ceed2a1c8ed15707ff514
yankscyt <yankscaburnay@gmail.com>
2025-12-06 14:30:18 +0800

    feat: Initial Spring Boot backend scaffolding

---
+++ backend/my-notes-app/src/main/java/com/yankee/my_notes_app/MyNotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.yankee.my_notes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNotesAppApplication.class, args);
+	}
+
+}
